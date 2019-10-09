00214c08c2195b43a599d318071dd1bb72a402a5
dendickys <febri.unquad2014@gmail.com>
2019-10-09 08:52:26 +0700

    Add Tabbed Layout

--- app/src/main/java/com/dendickys/moviecatalogueapi/MainActivity.java
+++ app/src/main/java/com/dendickys/moviecatalogueapi/MainActivity.java
@@ -1,8 +1,15 @@
 package com.dendickys.moviecatalogueapi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.dendickys.moviecatalogueapi.ui.main.SectionsPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "tada", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+            }
+        });
     }
 }
